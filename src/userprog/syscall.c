3f7bf684316fa4e9c38a18881ce3eaa7f778ffd6
jlsbz <937391832@qq.com>
2019-06-28 13:21:30 +0800

    a little bug

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -55,7 +55,7 @@ struct file_descriptor *get_fd(struct thread *t, int fd_num)
 static int num_of_args(const char *esp, int args[])
 {
   int argc;
-  boundary_check(argc + 3); //maybe check all is a must
+  boundary_check(esp + 3); //maybe check all is a must
 
   int sys_num = *(int *)esp;
   switch (sys_num)
@@ -252,8 +252,7 @@ int sys_read(int fd_num, void *buffer, unsigned size)
 {
   boundary_check(buffer);
   boundary_check((uint64_t *)buffer + size - 1);
-  if (fd_num == STDIN_FILENO) //there should be a enum that declare the stdin and out
-                              // but I dont find it yet
+  if (fd_num == STDIN_FILENO) //stdio.h
   {
     uint8_t *udst = (uint8_t *)buffer;
     for (unsigned i = 0; i < size; ++i)
@@ -290,7 +289,8 @@ int sys_write(int fd_num, const void *buffer, unsigned size)
   }
   if (fd_num == STDOUT_FILENO)
   {
-    putbuf((char *)buffer, size);
+    putbuf((char *)buffer, size);//in fact i dont find this function
+                                 //but others are all using it.
     return size;
   }
 
@@ -339,6 +339,40 @@ void sys_close(int fd_num)
   list_remove(&fd->elem);
   free(fd);
 }
+
+
+
+
+/* Reads a byte at user virtual address UADDR.
+   UADDR must be below PHYS_BASE.
+   Returns the byte value if successful, -1 if a segfault
+   occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+ 
+/* Writes BYTE to user address UDST.
+   UDST must be below PHYS_BASE.
+   Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
+
+
+
+
+
+
 /* modified by YN  end*/
 
 //printf ("system call!\n");
